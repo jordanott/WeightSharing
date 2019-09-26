3562a646baa5e9c90b2311e4539cae9eb52458ff
Jordan Ott <jordanott365@gmail.com>
2019-09-26 13:44:03 -0700

    Updates for paper

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# HyperOpt
-Hyperparameter optimization starter code
+# Learning in the Machine: To Share or not to Share?
+
+```
+@article{ott2019share,
+  title={Learning in the Machine: To Share or not to Share?},
+  author={Ott, Jordan and Linstead, Erik and LaHaye, Nicholas and Baldi, Pierre},
+  journal={arXiv preprint arXiv:1909.11483},
+  year={2019}
+}
+```
+
+# Pipeline
+1. [Hyperparameter grid search](https://github.com/jordanott/WeightSharing/tree/master/HyperOpt)
+  * Number of layers
+  * Learning rate
+2. [Full experiments](https://github.com/jordanott/WeightSharing/blob/master/runner.py)
+  * Types of augmentation
+    * Edge noise
+    * Translation
+    * Rotation
+    * Noise
+    * Quadrant swap
+3. [Results](https://github.com/jordanott/WeightSharing/blob/master/Results/Results.ipynb)
+  * Metrics
+    * Loss
+    * Validation loss
+    * Training accuracy
+    * Validation accuracy
+    * Edge noise augmented validation accuracy
+    * Translation augmented validation accuracy
+    * Noise augmented validation accuracy
+    * Rotation augmented validation accuracy
+  * Approximate weight sharing
+    * Observing the distance between free convolutional filters within a layer
+  * Variable Connection Patterns
+    * Varying the probability
--- Results/results.py
+++ Results/results.py
@@ -19,96 +19,45 @@ pbar = tqdm(total=19)
 # In[2]:
 
 from IPython.core.pylabtools import figsize
-figsize(20, 6)
+figsize(20, FIG_HEIGHT)
 
-
-# In[3]:
-
-
-# plt.rc('text', usetex=True)
-# plt.rc('font', family='serif', size=20)
-# matplotlib.rcParams['lines.linewidth'] = lw
-
+# LATEX BACKEND
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=FONTSIZE)
 
 # # MNIST
 # ---
 
 # In[6]:
 
+mnist, mnist_cnn, mnist_fcn, mnist_fcn_vcp = load_results('mnist')
+cifar, cifar_cnn, cifar_fcn, cifar_fcn_vcp = load_results('cifar')
 
-mnist_all = pd.read_csv('mnist.csv')
-mnist = mnist_all[mnist_all['aug_type'] != 'vcp']
-
-mnist_cnn = mnist[mnist['model'] == 'cnn'].reset_index()
-mnist_fcn = mnist[mnist['model'] == 'fcn'].reset_index()
-mnist_fcn_vcp = mnist_all[mnist_all['aug_type'] == 'vcp'].reset_index()
-
-
-# In[7]:
-
-
-convert_table(mnist, eval_type='translation', to_latex=False)
-
-
-# In[8]:
-
-
-convert_table_vcp(mnist_fcn_vcp)
-
-
-# # CIFAR
+# Paper Figures
 # ---
-
-# In[9]:
-
-
-cifar_all = pd.read_csv('cifar.csv').append(pd.read_csv('cifar_1.csv'), ignore_index=True)
-cifar = cifar_all[cifar_all['aug_type'] != 'vcp']
-
-cifar_cnn = cifar[cifar['model'] == 'cnn'].reset_index()
-cifar_fcn = cifar[cifar['model'] == 'fcn'].reset_index()
-cifar_fcn_vcp = cifar_all[cifar_all['aug_type'] == 'vcp'].reset_index()
-
-
-# In[10]:
-
-
-convert_table(cifar, eval_type='val_acc', to_latex=False)
-
-
-# In[11]:
-
-
-convert_table_vcp(cifar_fcn_vcp)
-
-
-# # Paper Figures
-# ---
-# ### 1. Translation (left: FCN; right: CNN)
-#     * Translation Augmented Training Acc
-#     * Un-Augmented Validation Acc
-#     * Translation Augmented Validation Acc
+### 1. Translation (left: FCN; right: CNN)
+#    * Translation Augmented Training Acc
+#    * Un-Augmented Validation Acc
+#    * Translation Augmented Validation Acc
 
 # In[13]:
 
-
-# translation_paper_fig(mnist_fcn, mnist_cnn)
-# pbar.update(1)
+translation_paper_fig(mnist_fcn, mnist_cnn)
+pbar.update(1)
 
 # In[14]:
-
 
 translation_paper_fig(cifar_fcn, cifar_cnn, cifar=True)
 pbar.update(1)
 
-# ### 2. Approximate Weight Sharing (left: distance @ 1; right: distance @ 4)
+### 2. Approximate Weight Sharing (left: distance @ 1; right: distance @ 4)
 
 # In[15]:
 
 
-# approx_ws(mnist_fcn, dist_type='dist', y=[.05, .12])
+approx_ws(mnist_fcn, dist_type='dist', y=[.05, .12])
 #approx_ws(mnist_fcn, dist_type='cos')
-# pbar.update(1)
+pbar.update(1)
 
 # In[16]:
 
@@ -121,116 +70,81 @@ pbar.update(1)
 
 # In[17]:
 
-plt.clf(); _=plt.figure(figsize=(10, 6));
-# plt.subplot(1,2,1)
-p = sns.color_palette('coolwarm', 11)
-sns.lineplot(x='epoch', y='swap', data=mnist_cnn, label='CNN',color=p[0]); sns.lineplot(x='epoch', y='swap', data=mnist_fcn, label='FCN',color=p[-1])
-plt.title('Feature Swap MNIST'); plt.xlabel('Epochs'); plt.ylabel('Accuracy'); plt.legend(shadow=True, title='Model');
+df = pd.read_csv('../FinalResults/mnist_1.csv')
+cnn = df[df['model'] == 'cnn']
+fcn = df[df['model'] == 'fcn']
 
-# plt.subplot(1,2,2)
-# sns.lineplot(x='epoch', y='swap', data=cifar_cnn, label='CNN',color=p[0]); sns.lineplot(x='epoch', y='swap', data=cifar_fcn, label='FCN',color=p[-1])
-# plt.title('(b) Feature Swap CIFAR'); plt.xlabel('Epochs'); plt.ylabel('Accuracy'); plt.legend(shadow=True, title='Model')
+plt.clf(); _=plt.figure(figsize=(10, FIG_HEIGHT));
+
+p = sns.color_palette('coolwarm', 11)
+sns.lineplot(x='epoch', y='swap', data=cnn, label='CNN',color=p[0]); sns.lineplot(x='epoch', y='swap', data=fcn, label='FCN',color=p[-1])
+plt.title('Feature Swap MNIST'); plt.xlabel('Epochs', fontsize=FONTSIZE); plt.ylabel('Accuracy', fontsize=FONTSIZE); plt.legend(shadow=True, title='Model', prop=legend_settings, fontsize=FONTSIZE);
+
 plt.tight_layout(); plt.savefig('Figures/Paper/swap.{save_type}'.format(
     save_type=SAVE_TYPE
 ))
 pbar.update(1)
 
-# # Supplementary Material Figures
+# Supplementary Material Figures
 # ---
-# ### 1. Augmentation Type 
-#     * (left: aug_type training, right: un-augmented val acc)
-#     * (left: translation val, right: rotation val)
-#     * (left: noise val, right: edge noise val)
-#     * swap val
+### 1. Augmentation Type
+#    * (left: aug_type training, right: un-augmented val acc)
+#    * (left: translation val, right: rotation val)
+#    * (left: noise val, right: edge noise val)
+#    * swap val
 
 # In[19]:
 
 
-# translation_sm_fig(mnist_cnn, aug_type='rotation', save=True)
-# pbar.update(1)
-#
-# # In[ ]:
-#
-#
-# translation_sm_fig(mnist_cnn, aug_type='noise', save=True)
-# pbar.update(1)
-#
-# # In[ ]:
-#
-#
-# translation_sm_fig(mnist_cnn, aug_type='edge_noise', save=True)
-# pbar.update(1)
-#
-# # In[ ]:
-#
-#
-# translation_sm_fig(mnist_fcn, aug_type='rotation', save=True)
-# pbar.update(1)
-#
-# # In[ ]:
-#
-#
-# translation_sm_fig(mnist_fcn, aug_type='noise', save=True)
-# pbar.update(1)
-#
-# # In[20]:
-#
-#
-# translation_sm_fig(mnist_fcn, aug_type='edge_noise', save=True)
-# pbar.update(1)
+translation_sm_fig(mnist_cnn, aug_type='rotation', save=True)
+pbar.update(1)
 
-# In[ ]:
+translation_sm_fig(mnist_cnn, aug_type='noise', save=True)
+pbar.update(1)
 
+translation_sm_fig(mnist_cnn, aug_type='edge_noise', save=True)
+pbar.update(1)
+
+translation_sm_fig(mnist_fcn, aug_type='rotation', save=True)
+pbar.update(1)
+
+translation_sm_fig(mnist_fcn, aug_type='noise', save=True)
+pbar.update(1)
+
+translation_sm_fig(mnist_fcn, aug_type='edge_noise', save=True)
+pbar.update(1)
 
 translation_sm_fig(cifar_cnn, aug_type='rotation', cifar=True, save=True)
 pbar.update(1)
 
-# In[ ]:
-
-
 translation_sm_fig(cifar_cnn, aug_type='noise', cifar=True, save=True)
 pbar.update(1)
-
-# In[21]:
-
 
 translation_sm_fig(cifar_cnn, aug_type='edge_noise', cifar=True, save=True)
 pbar.update(1)
 
-# In[ ]:
-
-
 translation_sm_fig(cifar_fcn, aug_type='rotation', cifar=True, save=True)
 pbar.update(1)
 
-# In[ ]:
-
-
 translation_sm_fig(cifar_fcn, aug_type='noise', cifar=True, save=True)
 pbar.update(1)
-
-# In[22]:
-
 
 translation_sm_fig(cifar_fcn, aug_type='edge_noise', cifar=True, save=True)
 pbar.update(1)
 
 # ### 2. Variable Connection Pattersn
 
-# In[23]:
-
-
-# translation_sm_fig(
-#     mnist_fcn_vcp[mnist_fcn_vcp['vcp'] == 0.1].reset_index(),
-#     aug_type='vcp'
-# )
-# pbar.update(1)
+translation_sm_fig(
+    mnist_fcn_vcp.reset_index(),
+    aug_type='vcp'
+)
+pbar.update(1)
 
 # In[24]:
 
 
 translation_sm_fig(
-    cifar_fcn_vcp[cifar_fcn_vcp['vcp'] == 0.1].reset_index(), 
+    cifar_fcn_vcp.reset_index(),
     aug_type='vcp',
     cifar=True
 )
